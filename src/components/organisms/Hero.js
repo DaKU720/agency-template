b76bd5a6a9399811d369d85c016daf5ad319a6c9
Alan Dakowski <alandakowski@gmail.com>
2025-03-04 19:46:34 +0100

    corrected font size, imgur images, margins

--- src/components/organisms/Hero.js
+++ src/components/organisms/Hero.js
@@ -6,37 +6,37 @@ export default function Hero() {
                 <p className="text-right">is king</p>
             </div>
 
-            <p className="self-stretch text-justify mt-10 text-2xl md:mt-6 md:text-4xl leading-relaxed tracking-wide">
+            <p className="text-xl pt-20 sm:text-2xl md:text-3xl mb-8 w-full font-bold">
                 Cras vehicula congue nunc a nisi vehicula interdum.
                 Vestibulum volutpat sapien sit amet amet vulputate justo.
                 Praesent id nullam aliquam justo sit amet nullam pharetra libero vulputate lorem pellentesque pulvinar.
             </p>
 
-            <div className="flex flex-col items-center mt-12 md:mt-14">
+            <div className="flex flex-col items-center mt-2">
                 <div className="flex flex-col md:flex-row justify-between gap-4 w-full">
                     <img
                         src="https://i.imgur.com/7iFovAj.png"
                         alt=""
-                        className="w-full md:w-[500px] rounded-3xl"
+                        className="w-full md:w-[500px] rounded-3xl transition-transform duration-300 ease-in-out transform hover:scale-105"
                     />
                     <img
                         src="https://i.imgur.com/3e2BLUC.png"
                         alt=""
-                        className="w-full md:w-[300px] rounded-3xl"
+                        className="w-full md:w-[300px] rounded-3xl transition-transform duration-300 ease-in-out transform hover:scale-105"
                     />
                     <img
                         src="https://i.imgur.com/c7CdzeI.png"
                         alt=""
-                        className="w-full md:w-[500px] rounded-3xl"
+                        className="w-full md:w-[500px] rounded-3xl transition-transform duration-300 ease-in-out transform hover:scale-105"
                     />
                 </div>
-                <p className="self-stretch text-justify mt-10 text-2xl md:mt-14 md:text-4xl leading-relaxed tracking-wide">
+                <p className="text-xl pt-20 sm:text-2xl md:text-3xl mb-2 w-full font-bold">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                     Curabitur malesuada tellus id erat interdum, nec scelerisque nisi cursus.
                     Praesent facilisis ligula vel diam vulputate tincidunt.
                 </p>
             </div>
-            <div className="flex flex-col md:flex-row gap-10 justify-center w-full mt-[5rem]">
+            <div className="flex flex-col md:flex-row gap-10 justify-center w-full mt-[3rem]">
                 <div className="flex-1 text-left leading-none">
                     <p className="text-[5rem] md:text-[8rem] font-bold">8</p>
                     <p className="text-left text-xl md:text-xl">Years of experience in Social Media</p>
