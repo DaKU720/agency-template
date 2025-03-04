38984cbbae6801953ac667be14e09299cecc543f
Alan Dakowski <alandakowski@gmail.com>
2025-03-04 21:27:16 +0100

    added custom footer, and small improvments

---
+++ src/components/organisms/ExtraFooter.js
@@ -0,0 +1,8 @@
+const extraFooter = () => {
+    return (
+        <div className="text-center font-extrabold text-sm mt-12">
+            Application Created by Bohdan & David in React.js
+        </div>
+    )
+}
+export default extraFooter;
--- src/layouts/PreviewLayout/PreviewLayout.js
+++ src/layouts/PreviewLayout/PreviewLayout.js
@@ -1,4 +1,3 @@
-import { Helmet } from "react-helmet-async";
 import {motion} from "framer-motion"
 
 import Navbar from "../../components/organisms/Navbar"
@@ -7,6 +6,7 @@ import Team from "../../components/organisms/Team";
 import Services from "../../components/organisms/Services";
 import Clients from "../../components/organisms/Clients";
 
+import ExtraFooter from '../../components/organisms/ExtraFooter'
 
 import "../../assets/scss/App.scss"
 
@@ -18,16 +18,6 @@ const PreviewLayout = () => {
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.5 }}
             >
-                {/* META TAGS */}
-                <Helmet>
-                    <title>Agency Template</title>
-                    <meta name="description" content="We build modern, fast, and responsive websites & apps." />
-                    <meta property="og:title" content="Daku Studio - Web & App Development" />
-                    <meta property="og:description" content="Check out our latest projects in web development and design!" />
-                    <meta property="og:image" content="https://dakustudio.com/images/preview-thumbnail.jpg" />
-                    <meta property="og:url" content="https://dakustudio.com" />
-                </Helmet>
-
                 {/* Website Structure */}
                 <div className="container mx-auto min-h-screen flex flex-col">
                     <Navbar />
@@ -35,6 +25,7 @@ const PreviewLayout = () => {
                     <Team />
                     <Services />
                     <Clients />
+                    <ExtraFooter />
                 </div>
             </motion.div>
         </>
